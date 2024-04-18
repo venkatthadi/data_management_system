6dbace2a03d82eccb4223a873094272224cc13d7
venkatthadi <venkat@mobicip.com>
2024-04-18 15:20:55 +0530

    bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_17_104743) do
+ActiveRecord::Schema.define(version: 2024_04_18_093126) do
 
   create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
